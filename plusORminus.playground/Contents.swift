1b5d19095bcd279fd5cdae48d6ba7804b8d3770e
Maciej Matyjas <mmatyjas@expedia.com>
2016-09-01 10:48:58 +0100

    add or subtract to get the right answer

---
+++ plusORminus.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Background problem: Insert + or - signs anywhere between the digits 123456789 in such a way that the expression evaluates to 100. The order of the digits must not be changed.
+
+//: Sample solution: 1 + 2 + 3 - 4 + 5 + 6 + 78 + 9 = 100
+
+//: Programming problem: Write a program to output all solutions.
+
+//: Source -> http://www.dgp.toronto.edu/~flaps/progprobs.html
+
+import Cocoa
+
+
